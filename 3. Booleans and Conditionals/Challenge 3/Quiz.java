89fca62082910c5af51bff6fb054b99ded3e20ca
Cedric Kamdem <kcedrico42@gmail.com>
2026-01-31 02:50:08 +0100

    resolution du challenge sur les structure conditionnelles!

--- 3. Booleans and Conditionals/Challenge 3/Quiz.java
+++ 3. Booleans and Conditionals/Challenge 3/Quiz.java
@@ -11,21 +11,32 @@ public class Quiz {
         System.out.println("1. Which country held the 2016 Summer Olympics?");
         System.out.println("\ta) China\n\tb) Ireland\n\tc) Brazil\n\td) Italy\n");
         //store answer 1
-
+        char response1 = scan.next().charAt(0);
         System.out.println("\n2. Which planet is the hottest?");
         System.out.println("\ta) Venus\n\tb) Saturn\n\tc) Mercury\n\td) Mars\n");
         //store answer 2
-
+        char response2 = scan.next().charAt(0);
         System.out.println("\n3. What is the rarest blood type?");
         System.out.println("\ta) O\n\tb) A\n\tc) B\n\td) AB-Negative\n");
         //store answer 3
-
+        char response3 = scan.next().charAt(0);
         System.out.println("\n4. Which one of these characters is friends with Harry Potter?");
         System.out.println("\ta) Ron Weasley\n\tb) Hermione Granger\n\tc) Draco Malfoy\n");
         //store answer 4
-
+        char response4 = scan.next().charAt(0);
         int score = 0;
-
+        if (response1 == 'c'){
+            score += 5;
+        }
+        if (response2 == 'a'){
+            score += 5;
+        }
+        if (response3 == 'd'){
+            score += 5;
+        }
+        if (response4 == 'a' || response4 == 'b'){
+            score += 5;
+        }
 
         //Task 2: Check each answer - For each correct answer, add 5 points to the score. 
         
@@ -34,6 +45,14 @@ public class Quiz {
         //Task 3: print a message depending on the score
 
         // if the score is 15 or higher, print: "Wow, you know your stuff!";
+        if (score >= 15){
+            System.out.println("Wow, you know your stuff!");
+        }
+        else if (score < 5){
+            System.out.println("better luck next time.");
+        }else {
+            System.out.println("Not bad!");
+        }
         // if the score is between 5 and 15, print "Not bad!";
         // else, better luck next time. 
 
--- 3. Booleans and Conditionals/Workbook 3.7/Bank.java
+++ 3. Booleans and Conditionals/Workbook 3.7/Bank.java
@@ -9,28 +9,35 @@ public class Bank {
         System.out.println("\n****ROYAL BANK OF JAVA****");
         System.out.println("Are you here to get a mortgage? (yes or no)");
         //Task 1 - Pick up the user's decision.
-
+        String decision = scan.nextLine();
 
         //Task 2 - Print this if the decision is "yes"
-        System.out.println("\nGreat! In one line" +
+        if (decision.equals("yes")){
+            System.out.println("\nGreat! In one line" +
             "\nHow much money do you have in your savings?" +
             "\nAnd, how much do you owe in credit card debt?");
-                // Task 3 - Pick up each value 
+            // Task 3 - Pick up each value 
+            int savingMoney = scan.nextInt();
+            int creditDebt = scan.nextInt();
             System.out.println("\nHow many years have you worked for?");
                // Task 4 - Pick up number of years
-
+            int workYears = scan.nextInt();
             System.out.println("What is your name?");
                // Task 5 - Pick up the user's name
-                
+            scan.nextLine();
+            String name = scan.nextLine();
                //Task 6 - Approve the mortgage if they meet the requirements (see article)
-               //       - Otherwise, don't give them a mortgage.          
-               System.out.println("Congratulations <name> You have been approved!");
-               System.out.println("Sorry, you are not eligible for a mortgage.");
-
-        //Task 2 - Print this if the decision was not "yes"
-        System.out.println("\nOK. Have a nice day!");
-
-
+               //       - Otherwise, don't give them a mortgage.       
+            if(savingMoney >= 10000 && creditDebt < 5000 && workYears >= 2){
+                System.out.println("Congratulations " + name + " You have been approved!");
+            }else{
+                System.out.println("Sorry, you are not eligible for a mortgage.");
+            }
+               
+        } else{
+            //Task 2 - Print this if the decision was not "yes"
+            System.out.println("\nOK. Have a nice day!");
+        }    
         scan.close();
     }
 }
