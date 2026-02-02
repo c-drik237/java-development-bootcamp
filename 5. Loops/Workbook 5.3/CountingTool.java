db1ebbe94dd069f3d0e557878fbca78dfa40a274
Cedric Kamdem <kcedrico42@gmail.com>
2026-02-02 02:04:39 +0100

    résolution du workbook 5.3

--- 5. Loops/Workbook 5.3/CountingTool.java
+++ 5. Loops/Workbook 5.3/CountingTool.java
@@ -5,6 +5,12 @@ public class CountingTool {
         Scanner scan = new Scanner(System.in);
 
         // See Learn the Part for the instructions.
+        System.out.print("Hi Timmy! Choose a number to count to:  ");
+        int number = scan.nextInt();
+        System.out.println("Great! Hers is how it is done");
+        for (int i = 0; i < number + 1; i++){
+            System.out.print(i + "  ");
+        }
 
         scan.close();
     }
