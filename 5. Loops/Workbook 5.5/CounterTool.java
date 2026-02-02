347b0811e5c66b86479b0f9b8dfa69743f105a70
Cedric Kamdem <kcedrico42@gmail.com>
2026-02-02 18:59:29 +0100

    résolution du workbook 5.5

--- 5. Loops/Workbook 5.5/CounterTool.java
+++ 5. Loops/Workbook 5.5/CounterTool.java
@@ -8,7 +8,15 @@ public class CounterTool {
         System.out.println("Oh, ok...");
 
        // See detailed instructions on Learn the Part.
-
+       System.out.println("1. Pick a number to count by: ");
+       int step = scan.nextInt();
+       System.out.println("2. Pick a number to start counting from: ");
+       int start = scan.nextInt();
+       System.out.println("3. Pick a number to count to: ");
+       int end = scan.nextInt();
+       for (int i = start; i <= end; i+=step){
+        System.out.print(i + "  ");
+       }
         scan.close();
     }
 }
