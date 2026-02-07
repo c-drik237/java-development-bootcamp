3ca4906eb424c683593e0ff7e167a1e617a8ac97
Cedric Kamdem <kcedrico42@gmail.com>
2026-02-07 16:13:10 +0100

    résolution du workbook 5.11

--- 5. Loops/Workbook 5.11/JavaRolls.java
+++ 5. Loops/Workbook 5.11/JavaRolls.java
@@ -7,11 +7,45 @@ public class JavaRolls {
         
         int diceRoll;
         int score = 0;
-
+        System.out.println("Let's play Rolling Java. Type anything to start.");
+        scan.nextLine();
+        System.out.println("Great, here are the rules:\n");
+        System.out.println("- If you roll a 4, nothing happens.");
+        System.out.println("- Otherwise, you get 1 point.\n");
+        System.out.println("You must collect at least 3 points to win. Enter anything to roll:");
+    
+        while(true){ 
+            scan.nextLine();
+            diceRoll = rollDice();
+            System.out.println("You rolled a "+ diceRoll);
+            if (diceRoll == 6){
+                System.out.println("End of game");
+                break;
+            }else if (diceRoll == 4){
+                System.out.println("Zero points. Keep rolling.");
+            }else{
+                score += 1;
+                System.out.println("One point. Keep rolling");
+            }
+        }
+        if(score >= 3){
+            System.out.println("Wow, that's lucky. You win!");
+        }else{
+            System.out.println("Tough luck, you lose :(");
+        }
     }
-  
-  
 
-  
-  
+    /**
+     * Function name: rollDice
+     * 
+     * @return randomNumber (int)
+     *
+     *         Inside the function:
+     *         - return a random number between one and six.
+     */
+
+    public static int rollDice() {
+        return (int) (Math.random() * 6 + 1);
+    }
+
 }
