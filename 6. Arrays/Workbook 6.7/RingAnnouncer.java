326ba6c2d36aef089a17530325f52be5596c2b23
Cedric Kamdem <kcedrico42@gmail.com>
2026-02-15 15:52:06 +0100

    Résolution du workbook 6.7

--- 6. Arrays/Workbook 6.7/RingAnnouncer.java
+++ 6. Arrays/Workbook 6.7/RingAnnouncer.java
@@ -3,10 +3,16 @@ public class RingAnnouncer {
         String[] record = {"WIN", "WIN", "WIN", "LOSS", "WIN", "WIN", "LOSS"};
         int wins = 0;
         int losses = 0;
-        
+        for (String result : record){
+            if(result.equals("WIN")){
+                wins++;
+            }else{
+                losses++;
+            }
+        }
         // Instructions for this workbook are on Learn the Part (Workbook 6.7).
         
-        System.out.println("\nWith a professional record of <wins> Wins and <losses> losses.");
+        System.out.println("\nWith a professional record of " + wins + " Wins and " + losses + " losses.");
         System.out.println("He is the pride of oracle: Java Fury!");
 
 
