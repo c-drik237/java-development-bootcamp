e3ff581b4317337d4bb38e77d957138fda0791e0
Cedric Kamdem <kcedrico42@gmail.com>
2026-02-15 13:59:54 +0100

    Résolution du workbook 6.4

--- 6. Arrays/Workbook 6.4/Grocer.java
+++ 6. Arrays/Workbook 6.4/Grocer.java
@@ -10,11 +10,14 @@ public class Grocer {
         String[] store = {"apples", "bananas", "candy", "chocolate", "coffee", "tea"};
         System.out.println("\nWelcome to Java Grocers. ");
         System.out.println("What can I help you find?\n");
-        
+        String res = scan.nextLine();
         for (int i = 0; i < store.length; i++) {
+            if (res.equals(store[i])){
+                // Task 2 here
+                System.out.println("\nWe have that in aisle: " + i);
+                break;
+            }
             
-            // Task 2 here
-            System.out.println("\nWe have that in aisle: <index>");
 
 
         }
