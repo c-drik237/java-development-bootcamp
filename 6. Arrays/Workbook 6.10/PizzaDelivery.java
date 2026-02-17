85ce175fb8a576b66d99a0b20c20c3f1f978e5b2
Cedric Kamdem <kcedrico42@gmail.com>
2026-02-17 10:28:32 +0100

    Résolution du workbook 6.10

--- 6. Arrays/Workbook 6.10/PizzaDelivery.java
+++ 6. Arrays/Workbook 6.10/PizzaDelivery.java
@@ -14,13 +14,18 @@ public class PizzaDelivery {
         System.out.println("Great, enter each topping!");
         
         String[] toppings = new String[numToppings]; // Length of array equals what the user inputs.
-        
         // Task 1 here
+        for (int i = 0; i < toppings.length; i++){
+            System.out.print(i + ". ");
+            toppings[i] = scan.nextLine();
+        }
         
         System.out.println("\nThank you! Here are the toppings you ordered");
         
         // Task 2 here
-        
+        for (int i = 0; i < toppings.length; i++){
+            System.out.println(i + ". " + toppings[i]);
+        }
         System.out.println("\nPress anything to confirm your order.");
         scan.nextLine();
         System.out.println("Great, a driver is on the way!");
