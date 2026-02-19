74e90a473f3821a525af7935c997e3e40f9bc40f
Cedric Kamdem <kcedrico42@gmail.com>
2026-02-19 12:29:40 +0100

    Résolution du workbook 7.2

--- 7. The World of Objects/Workbook 7.2/Person.java
+++ 7. The World of Objects/Workbook 7.2/Person.java
@@ -5,5 +5,13 @@ public class Person {
     String dateOfBirth;
     String[] passport;
     int seatNumber;
+
+    public Person(String name, String nationality, String dateOfBirth, int seatNumber) {
+        // set fields equal to parameters here.
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.nationality = nationality;
+        this.seatNumber = seatNumber;
+    }
  
 }
