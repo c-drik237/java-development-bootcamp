6c1e7691023b0aa43dcf3ab8c9d71b735dabf4bf
Cedric Kamdem <kcedrico42@gmail.com>
2026-02-21 11:49:27 +0100

    Résolution du workbook 7.5

--- 7. The World of Objects/Workbook 7.5/Person.java
+++ 7. The World of Objects/Workbook 7.5/Person.java
@@ -53,5 +53,28 @@ public class Person {
         this.seatNumber = seatNumber;
     }
 
+    /**
+ *  Function name: applyPassport
+ *  @return (boolean)
+ *
+ *  Inside the function:
+ *     1. Returns a random boolean of true or false.
+ */
+
+    public boolean applyPassport(){
+        return (int)(Math.random() * 2) == 1;
+    }
+
+    /**
+ *  Function name: chooseSeat
+ *
+ *  Inside the function:
+ *    1. Sets this.seat to a random number between 1 -- 11.
+ */
+
+    public void chooseSeat(){
+        this.seatNumber = (int)((Math.random() * 11) + 1);
+    }
+
  
 }
