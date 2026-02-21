50f088ec3d2537f73c2b1ae38542f7d6ae72bd65
Cedric Kamdem <kcedrico42@gmail.com>
2026-02-21 13:21:55 +0100

    Résolution du workbook 7.6

--- 7. The World of Objects/Workbook 7.6/Person.java
+++ 7. The World of Objects/Workbook 7.6/Person.java
@@ -13,6 +13,7 @@ public class Person {
         this.nationality = nationality;
         this.dateOfBirth = dateOfBirth;
         this.seatNumber = seatNumber;
+        this.passport = new String[3];
     }
 
     public Person(Person source) {
@@ -55,8 +56,14 @@ public class Person {
         this.seatNumber = seatNumber;
     }
 
-    public void setPassport() {
+    public String[] getPassport() {
+        return Arrays.copyOf(this.passport, this.passport.length);
+    }
 
+    
+
+    public void setPassport() {
+        this.passport = new String[] {this.name, this.nationality, this.dateOfBirth};
     }
 
     public boolean applyPassport() {
