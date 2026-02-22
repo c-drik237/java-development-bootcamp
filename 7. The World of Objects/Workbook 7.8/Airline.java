0eeb80f5dd5bf74d6754e6b38dd909532502b561
Cedric Kamdem <kcedrico42@gmail.com>
2026-02-22 15:54:22 +0100

    Résolution du workbook 7.8

--- 7. The World of Objects/Workbook 7.8/Airline.java
+++ 7. The World of Objects/Workbook 7.8/Airline.java
@@ -1,6 +1,20 @@
 public class Airline {
     
     Person[] people; //array that stores Person objects...
+    
+    public Airline(){
+        this.people = new Person[11];
+    }
+
+    public Person getPerson(int index){
+        return new Person(people[index]);
+    }
+
+    public void setPerson(Person person){
+        people[person.getSeatNumber() - 1] = new Person(person);
+    }
+
+
 
 
 }
